4fef6c6ba465c396e1dea53178eea4f4981cbd05
Duncan Ralph <dkralph@gmail.com>
2015-04-27 07:33:11 -0700

    enable history plots

--- upload-forecast.sh
+++ upload-forecast.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git pull origin master
-./scrape.py --no-history --outfname psathyrella.github.io/weatherscraper/weather.html
+./scrape.py --outfname psathyrella.github.io/weatherscraper/weather.html
 cd psathyrella.github.io
 git add --all weatherscraper/
 git commit -m "forecast for `date`"
