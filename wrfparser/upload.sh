243a596521d161d60d8b6e8fefab7b83cf39049a
duncan ralph <dkralph@gmail.com>
2021-06-14 15:44:17 -0700

    update upload.sh

--- wrfparser/upload.sh
+++ wrfparser/upload.sh
@@ -5,10 +5,12 @@ if [ "$repodir" == "" ] || ! [ -d $repodir ]; then
     echo "bad git repo dir $repodir"
     exit 1
 fi
+reponame=`basename $repodir`  # psathyrella.github.io
 
 echo "  pull/add/commit/push in $repodir"
 # TODO need to add the branch orphaning or whatever to this so it happens automatically every 100 commits or whatever (currently the .git dir just gets bigger and bigger, and is like 70GB after a year)
 cd $repodir
+
 git pull --quiet origin master
 git checkout --orphan tmp-branch
 git add --all wrfparser/  # Add all files and commit them
@@ -16,4 +18,11 @@ git commit --quiet -m "forecasts pushed on `date`"
 git branch -D master  # Deletes the master branch
 git branch -m master  # Rename the current branch to master
 git push --quiet -f origin master  # Force push master branch to github
-git gc --aggressive --prune=all     # remove the old files
+# git gc --aggressive --prune=all     # remove the old files
+
+cd .. #$OLDPWD
+if [ -d repo.bak/$reponame ]; then
+    rm -rf repo.bak/$reponame
+fi
+mv $reponame repo.bak/
+git clone --quiet git@github.com:psathyrella/$reponame
